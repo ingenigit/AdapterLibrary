a438cc1b93b9e42b3aa882052339c6f8f3e2691a
Suman Bahadur Shrestha <44489993+SumanBahadurShrestha@users.noreply.github.com>
2023-07-19 16:32:13 +0545

    remove image in feedbackadapter

--- adapter/src/main/java/com/or2go/adapter/FeedbackAdapter.java
+++ adapter/src/main/java/com/or2go/adapter/FeedbackAdapter.java
@@ -59,25 +59,25 @@ public class FeedbackAdapter extends RecyclerView.Adapter<FeedbackAdapter.ViewHo
         holder.textViewName.setText(item.getName());
         holder.textViewDesc.setText("");
         holder.imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-        RequestOptions options = new RequestOptions()
-                .placeholder(R.drawable.blankitem)
-                .error(R.drawable.blankitem);
-        if(item.getImagePath() == 0){
-            System.out.println(OR2GO_SERVER+"prodimage/"+prodNameToImagePath(item.getBrandName(), item.getName()) + ".jpg");
-            Glide.with(mContext)
-                    .load(OR2GO_SERVER+"prodimage/"+prodNameToImagePath(item.getBrandName(), item.getName()) + ".jpg")
-                    .apply(options)
-                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
-                    .into(holder.imageView);
-        }else if (item.getImagePath() == 1){
-            System.out.println(OR2GO_SERVER+"vendorprodimage/"+vendorId+"/"+item.getId()+ ".jpg");
-            Glide.with(mContext)
-                    .load(OR2GO_SERVER+"vendorprodimage/"+vendorId+"/"+item.getId()+ ".jpg")
-                    .apply(options)
-                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
-                    .into(holder.imageView);
-        }else
-            Toast.makeText(mContext, "No Product Image", Toast.LENGTH_SHORT).show();
+//        RequestOptions options = new RequestOptions()
+//                .placeholder(R.drawable.blankitem)
+//                .error(R.drawable.blankitem);
+//        if(item.getImagePath() == 0){
+//            System.out.println(OR2GO_SERVER+"prodimage/"+prodNameToImagePath(item.getBrandName(), item.getName()) + ".jpg");
+//            Glide.with(mContext)
+//                    .load(OR2GO_SERVER+"prodimage/"+prodNameToImagePath(item.getBrandName(), item.getName()) + ".jpg")
+//                    .apply(options)
+//                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+//                    .into(holder.imageView);
+//        }else if (item.getImagePath() == 1){
+//            System.out.println(OR2GO_SERVER+"vendorprodimage/"+vendorId+"/"+item.getId()+ ".jpg");
+//            Glide.with(mContext)
+//                    .load(OR2GO_SERVER+"vendorprodimage/"+vendorId+"/"+item.getId()+ ".jpg")
+//                    .apply(options)
+//                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+//                    .into(holder.imageView);
+//        }else
+//            Toast.makeText(mContext, "No Product Image", Toast.LENGTH_SHORT).show();
         holder.ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
